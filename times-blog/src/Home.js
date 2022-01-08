42a07e8a55b89952728ca6dd69b7c66746255bee
Ahmed Salem <ahmedsalem.me.as@gmail.com>
2022-01-08 14:48:55 +0200

    times-blog

---
+++ times-blog/src/Home.js
@@ -0,0 +1,19 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+
+const Home = () => {
+
+    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs');
+
+    return (
+        <div className="content">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blog && <BlogList blogs={blog} title='All Blogs' />}
+
+        </div>
+    );
+}
+
+export default Home; 
