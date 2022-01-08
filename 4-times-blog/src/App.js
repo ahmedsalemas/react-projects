3152139325dc04bb8bf3f4b972af0ad19d0629b1
Ahmed Salem <ahmedsalem.me.as@gmail.com>
2022-01-08 20:52:32 +0200

    times-blog

--- 4-times-blog/src/App.js
+++ 4-times-blog/src/App.js
@@ -1,37 +1,37 @@
 
-import Navbar from './Navbar';
-import Home from './Home';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar/>
-     
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
-          <Home/>
-          </Route>
-          <Route exact path="/create">
-          <Create/>
-          </Route>
-          <Route exact path="/blogs/:id">
-          <BlogDetails/>
-          </Route>
-          <Route  path="*">
-          <NotFound/>
-          </Route>
-        </Switch>
-       
-      </div>
-      
+      <div className="App">
+        <Navbar />
 
-    </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+
+        </div>
+
+
+      </div>
     </Router>
   );
 }
