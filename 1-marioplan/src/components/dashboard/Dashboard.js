9cf4f2b49bb0d098cbae397c063927e30f7794ea
Ahmed Salem <ahmedsalem.me.as@gmail.com>
2022-01-10 18:17:28 +0200

    fix

--- 1-marioplan/src/components/dashboard/Dashboard.js
+++ 1-marioplan/src/components/dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import ProjectList from "../projects/ProjectList";
-import Notifications from "./Notifications";
 import { Navigate } from "react-router-dom";
 
 
@@ -21,7 +20,6 @@ const Dashboard = ({ projects, auth }) => {
                     <ProjectList projects={projects} />
                 </div>
                 <div className="col s12 m5 offset-m1">
-                    <Notifications />
                 </div>
             </div>
         </div>
